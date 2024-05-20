a29cd5e8c66617780c8b09d86e6b8e9b0cc0ba6e
Siebe Burgerhout <siebe.burgerhout@icloud.com>
2024-05-20 14:03:58 +0200

    dashboard basics

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 import SignInButton from "@/components/auth/SignInButton";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
-
+import Dashboard from "@/components/navigation/Dashboard";
 
 export default async function Home() {
-
-  const session = await getServerSession(authOptions);  
+  const session = await getServerSession(authOptions);
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
-      <SignInButton />
+    <main className="flex min-h-screen flex-col">
+      {/* <SignInButton /> */}
+      <Dashboard />
     </main>
   );
 }
---
+++ frontend/src/components/navigation/Dashboard.tsx
@@ -0,0 +1,218 @@
+"use client";
+
+import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+import {
+  Home,
+  LineChart,
+  Menu,
+  Package,
+  Package2,
+  Bus,
+  Users,
+  CircleUser,
+} from "lucide-react";
+
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+
+const Allowed = {
+  ["ADMIN"]: ["Dashboard", "Preparatie", "Producten", "Gebruikers", "Analytica"],
+  ["STOCKMANAGER"]: ["Dashboard", "Preparatie", "Producten", "Analytica"],
+  ["USER"]: ["Dashboard", "Preparatie", "Producten"],
+}
+
+
+export default function Dashboard() {
+  const { data: session } = useSession();
+  console.log(session);
+
+  if (!session) {
+    return (
+      <div className="flex flex-col w-full h-screen items-center justify-center">
+        <h1>Je bent niet ingelogd</h1>
+        <Button onClick={() => signIn()}>
+            Log in
+        </Button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <div className="hidden border-r bg-muted/40 md:block">
+        <div className="flex h-full max-h-screen flex-col gap-2">
+          <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+            <Link href="/" className="flex items-center gap-2 font-semibold">
+              <Package2 className="h-6 w-6" />
+              <span className="">Vooraadmodule</span>
+            </Link>
+          </div>
+          <div className="flex-1">
+            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <Home className="h-4 w-4" />
+                Dashboard
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <Bus className="h-4 w-4" />
+                Preparatie
+                <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                  6
+                </Badge>
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-primary transition-all hover:text-primary"
+              >
+                <Package className="h-4 w-4" />
+                Producten
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <Users className="h-4 w-4" />
+                Gebruikers
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+              >
+                <LineChart className="h-4 w-4" />
+                Analytica
+              </Link>
+            </nav>
+          </div>
+          <div className="w-full flex items-center gap-4 pl-4 py-2 mb-4 lg:px-6 border-t">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="secondary"
+                  size="icon"
+                  className="rounded-full"
+                >
+                  <CircleUser className="h-5 w-5" />
+                  <span className="sr-only">Toggle user menu</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>Mijn account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Instellingen</DropdownMenuItem>
+                <DropdownMenuItem>Support</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Uitloggen</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <p className="text-muted-foreground text-sm">
+              Hallo, {session?.user.name}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col">
+        {/* MOBILE (Sheet) */}
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button
+              variant="outline"
+              size="icon"
+              className="shrink-0 md:hidden absolute top-2.5 right-2.5"
+            >
+              <Menu className="h-5 w-5" />
+              <span className="sr-only">Toggle navigation menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="right" className="flex flex-col">
+            <nav className="grid gap-2 text-lg font-medium">
+              <Link
+                href="#"
+                className="flex items-center gap-2 text-lg font-semibold"
+              >
+                <Package2 className="h-6 w-6" />
+                <span className="sr-only">Vooraadmodule</span>
+              </Link>
+              <Link
+                href="#"
+                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+              >
+                <Home className="h-5 w-5" />
+                Dashboard
+              </Link>
+              <Link
+                href="#"
+                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
+              >
+                <Bus className="h-5 w-5" />
+                Preparatie
+                <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                  6
+                </Badge>
+              </Link>
+              <Link
+                href="#"
+                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+              >
+                <Package className="h-5 w-5" />
+                Products
+              </Link>
+              <Link
+                href="#"
+                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+              >
+                <Users className="h-5 w-5" />
+                Customers
+              </Link>
+              <Link
+                href="#"
+                className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
+              >
+                <LineChart className="h-5 w-5" />
+                Analytics
+              </Link>
+            </nav>
+          </SheetContent>
+        </Sheet>
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+          <div className="flex items-center">
+            <h1 className="text-lg font-semibold md:text-2xl">Inventaris</h1>
+          </div>
+          <div
+            className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
+            x-chunk="dashboard-02-chunk-1"
+          >
+            <div className="flex flex-col items-center gap-1 text-center">
+              <h3 className="text-2xl font-bold tracking-tight">
+                Je hebt nog geen producten.
+              </h3>
+              <p className="text-sm text-muted-foreground">
+                Je kan beginnen met het managen van je voorraad door producten
+                toe te voegen.
+              </p>
+              <Button className="mt-4">Product toevoegen</Button>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
