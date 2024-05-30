a7fa0cbb29d41d320ea78cb0d5674bcbf6e64004
Siebe Burgerhout <siebe.burgerhout@icloud.com>
2024-05-30 12:39:04 +0200

    category + auth performance improvement

--- frontend/src/app/Providers.tsx
+++ frontend/src/app/Providers.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth";
+import { SessionProvider} from "next-auth/react";
 
-const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+const Providers = ({ children, session }: { children: React.ReactNode, session: Session }) => {
+  console.log(session);
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 };
 
 export default Providers;
