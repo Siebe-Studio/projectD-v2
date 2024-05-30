53bea31a7bc2f800bec9c455d5978c44c824e285
Zdennick <LombexTV@gmail.com>
2024-05-30 14:35:14 +0200

    Fixed CSS

--- frontend/src/app/locations/page.tsx
+++ frontend/src/app/locations/page.tsx
@@ -2,7 +2,7 @@ import { GetCategoryTabs } from "@/components/locations/CategoryTabs"
 
 export default async function Location() {
     return (
-        <div className="w-full">
+        <div className="flex flex-col w-full h-full max-h-full p-4 gap-6">
         <GetCategoryTabs/>
         </div>
     )
--- frontend/src/app/warehouse/[id]/page.tsx
+++ frontend/src/app/warehouse/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { WarehouseTable } from "@/components/warehouse/Warehouse"
 export default function WarehousePage({params}: {params: { id: string}}) {
     return (
-        <div>
+        <div className="flex flex-col w-full h-full max-h-full p-4 gap-6">
             <h1>WarehousePage: {params.id}</h1>
             <WarehouseTable/>
         </div>
